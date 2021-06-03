abff16e9b44d86463ced9afc47871eaaaa3850d6
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-02 22:06:41 -0300

    Apresentar todos os números divisíveis por 4 que sejam menores que 100

---
+++ mostraNumerosDivisiveis4Menores100.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+main(){
+	int x;
+	for(x = 1; x < 100; x++)
+		if (x % 4 == 0)
+			printf("%d\n",x);
+	
+	system("pause");
+	return 0;
+}
