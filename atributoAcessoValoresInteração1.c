9dd6b34d9a06ad482c496a19a680ff8eaac9afd8
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-25 06:46:40 -0300

    Atribuição e acesso a valores com estrutura de iteração 1

---
+++ atributoAcessoValoresInteração1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int i, lista[5];
+	
+	//lendo os valores
+	for (i=0; i<5; i++) {
+		printf("Informe um numero: \n");
+		scanf("%d", &lista[i]);
+	}
+	
+	//inicializando o maior valor
+	int maior = lista[0];
+	//encontrando o maior valor
+	for (i=1; i<5; i++) {
+		if (maior < lista[i])
+		maior = lista[i];
+	}
+	printf("Maior = %d \n", maior);
+}
