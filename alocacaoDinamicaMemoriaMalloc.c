46775dbc0ab94f926af6ec573af7994fb39d6746
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-26 18:13:07 -0300

    Alocação dinamica de Memoria com Malloc

---
+++ alocacaoDinamicaMemoriaMalloc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int *p;
+	p = (int*) malloc(10 * sizeof(int));
+	if (p == NULL) {
+		printf("Erro: Memoria insuficiente!\n");
+	}
+		else {
+			int i;
+			for (i=0; i<10; i++) {
+				printf("Informe um valor: \n");
+				scanf("%d", &p[i]);
+		}
+	}
+}
