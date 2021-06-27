7ae332df40057621c0e4e9c5afd24934953df2b6
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-27 13:18:07 -0300

    Programa Mostra a distância percorrida e a quantidade de litros consumidos na viagem.

---
+++ distanciaPercorrigaGastosConsumoEmLitrosCombustivel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+inte main(){
+	float tempoDecorrido, velocidadeMedia, distanciaPercorrida, combustivel;
+	
+	printf("Digite o tempo de viagem em horas: ");
+	scanf("%f",&tempoDecorrido);
+	
+	printf("Digite a velocidade media: ");
+	scanf("%f",&velocidadeMedia);
+	
+	distanciaPercorrida = tempoDecorrido * velocidadeMedia;
+	combustivel = distanciaPercorrida / 12;
+	
+	printf("Quantidade de litros consumidos: %f", combustivel);
+}
